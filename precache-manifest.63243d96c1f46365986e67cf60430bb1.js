f72b7f843197b8ef985439d82fe3483f1c060084
Dhruv Patel <dhruv.patel1@yahoo.com>
2020-08-16 16:11:45 -0400

    Updates

---
+++ precache-manifest.63243d96c1f46365986e67cf60430bb1.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f887be90000c0fe37a6d7cffb9e1684",
+    "url": "/Meal-Scouter/index.html"
+  },
+  {
+    "revision": "5c65b3815b07e3f7cc19",
+    "url": "/Meal-Scouter/static/css/main.79db963e.chunk.css"
+  },
+  {
+    "revision": "57b5d03ce38dccf98dc7",
+    "url": "/Meal-Scouter/static/js/2.9b6a7e40.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/Meal-Scouter/static/js/2.9b6a7e40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c65b3815b07e3f7cc19",
+    "url": "/Meal-Scouter/static/js/main.a175430b.chunk.js"
+  },
+  {
+    "revision": "51e57fe1ab02507a70f7",
+    "url": "/Meal-Scouter/static/js/runtime-main.46a875cf.js"
+  },
+  {
+    "revision": "eb934556f69d4b2eb111126109bfb5aa",
+    "url": "/Meal-Scouter/static/media/burger.eb934556.jpg"
+  },
+  {
+    "revision": "351b7060d2f50fe172d47b69dd25dd27",
+    "url": "/Meal-Scouter/static/media/caribbean.351b7060.jpg"
+  },
+  {
+    "revision": "1288001c3d7b60b998e849c5c8937cb8",
+    "url": "/Meal-Scouter/static/media/chicken.1288001c.jpg"
+  },
+  {
+    "revision": "522e2f5bb02cdd1c19774a8d6c04cfb3",
+    "url": "/Meal-Scouter/static/media/chinese.522e2f5b.jpg"
+  },
+  {
+    "revision": "d8be246ea10ffb6449f6cc15fd4be785",
+    "url": "/Meal-Scouter/static/media/filipino.d8be246e.jpg"
+  },
+  {
+    "revision": "e0599240016577c504558520a5b0071b",
+    "url": "/Meal-Scouter/static/media/french.e0599240.jpg"
+  },
+  {
+    "revision": "5be383f1e5bc86a0ab59fa35e48cd3b4",
+    "url": "/Meal-Scouter/static/media/greek.5be383f1.jpg"
+  },
+  {
+    "revision": "9001dd9fa3dd9205e242615789b1effa",
+    "url": "/Meal-Scouter/static/media/halal.9001dd9f.jpg"
+  },
+  {
+    "revision": "83950974e6cd062114bc4e7400c0f9cd",
+    "url": "/Meal-Scouter/static/media/homepic.83950974.svg"
+  },
+  {
+    "revision": "defd8a8c9d89fbae6fff509e6f26b2f6",
+    "url": "/Meal-Scouter/static/media/indian.defd8a8c.jpg"
+  },
+  {
+    "revision": "bb270c2a583d45125b35d8e4067c8281",
+    "url": "/Meal-Scouter/static/media/italian.bb270c2a.jpg"
+  },
+  {
+    "revision": "55d3b2e12c8b7fccb97e28e2b16a66a4",
+    "url": "/Meal-Scouter/static/media/japanese.55d3b2e1.jpg"
+  },
+  {
+    "revision": "f4cbf5c330429f1639d49aeb07a63347",
+    "url": "/Meal-Scouter/static/media/korean.f4cbf5c3.jpg"
+  },
+  {
+    "revision": "6b71dff3c1ca363c89eccb31f7b4c59f",
+    "url": "/Meal-Scouter/static/media/mexican.6b71dff3.jpg"
+  },
+  {
+    "revision": "62cfe694c1279ca11851e861bc1a0149",
+    "url": "/Meal-Scouter/static/media/new-logo.62cfe694.svg"
+  },
+  {
+    "revision": "ebf39d5686942b030ff64f92af43c3ac",
+    "url": "/Meal-Scouter/static/media/pizza.ebf39d56.jpg"
+  },
+  {
+    "revision": "7762bf43dc69577954c101f6611af21a",
+    "url": "/Meal-Scouter/static/media/sandwich.7762bf43.jpg"
+  },
+  {
+    "revision": "e4531475c4c9f95c154c28be33651c00",
+    "url": "/Meal-Scouter/static/media/spanish.e4531475.jpg"
+  },
+  {
+    "revision": "6e0297c24ab6295b28d3fb629ef86e6c",
+    "url": "/Meal-Scouter/static/media/thai.6e0297c2.jpg"
+  },
+  {
+    "revision": "f9d4f33824b005f4f0afeaaf72fc8242",
+    "url": "/Meal-Scouter/static/media/viet.f9d4f338.jpg"
+  },
+  {
+    "revision": "cad7008135a58fda8ec20be42808a6b2",
+    "url": "/Meal-Scouter/static/media/x.cad70081.svg"
+  }
+]);
